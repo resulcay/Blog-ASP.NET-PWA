ae049e4b0c5bd61af4d2499035efda2e035dd376
Resül Çay <resulcayop@gmail.com>
2024-01-22 22:37:05 +0300

    minor cahnges

--- Core/Models/UserUpdateViewModel.cs
+++ Core/Models/UserUpdateViewModel.cs
@@ -4,6 +4,8 @@
     {
         public string NameSurname { get; set; }
 
+        public string UserName { get; set; }
+
         public string Email { get; set; }
 
         public string ImageUrl { get; set; }
