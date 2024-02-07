6d21fbda143d6d67f707579a8ce8516144bbb6cd
Resül Çay <resulcayop@gmail.com>
2024-02-07 04:36:45 +0300

    newsletter minor bug fix

--- Core/Controllers/NewsLetterController.cs
+++ Core/Controllers/NewsLetterController.cs
@@ -1,13 +1,14 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
+    [AllowAnonymous]
     public class NewsLetterController : Controller
     {
-
         readonly NewsLetterManager newsLetterManager = new(new EfNewsLetterRepository());
 
         [HttpGet]
