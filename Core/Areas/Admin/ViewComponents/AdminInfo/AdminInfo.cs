8905fe6b10d3dbfd57b073d547583d6273333340
Resül Çay <resulcayop@gmail.com>
2024-02-21 17:42:44 +0300

    general minor changes and routing corrections

---
+++ Core/Areas/Admin/ViewComponents/AdminInfo/AdminInfo.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Core.Areas.Admin.ViewComponents.AdminInfo
+{
+    public class AdminInfo : ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AdminInfo(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var user = GetUserFromSession().Result;
+            return View(user);
+        }
+
+        private async Task<User> GetUserFromSession()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            return user;
+        }
+    }
+}
